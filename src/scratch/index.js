1072d77c0b9b6012625a497074c504e03e364352
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-07-17 18:42:12 +0100

    worked on understanding the need for a state machine

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -3,6 +3,8 @@ import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
 export const ScratchApp = () => {
+    const [isActive, setIsActive] = React.useState(false);
+
   return (
     <div className="scratch">
       <div className="alarm">
@@ -12,7 +14,7 @@ export const ScratchApp = () => {
             minute: '2-digit',
           })}
         </div>
-        <div className="alarmToggle" data-active></div>
+        <div className="alarmToggle" data-active={isActive || undefined} onClick={() => setIsActive(!isActive)}></div>
       </div>
     </div>
   );
