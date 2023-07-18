c0698bc47b5fe62c57cc94cb36c32c71343cfea7
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-07-18 23:43:06 +0100

    I did the timer loading thing with useReducer

--- src/scratch/index.js
+++ src/scratch/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
+const alarmMachine = {
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: {
+                TOGGLE: 'pending',
+            },
+        },
+        pending: {
+            on: {
+                SUCCESS: 'active',
+                TOGGLE: 'inactive',
+            }
+        },
+        active: {
+            on: {
+                TOGGLE: 'inactive',
+            },
+        },
+    },
+};
+const alarmReducer = (state, event) => {
+    return alarmMachine.states[state].on[event.type] || state;
+};
+
 export const ScratchApp = () => {
-    const [isActive, setIsActive] = React.useState(false);
+   const [status, dispatch] = useReducer(alarmReducer, alarmMachine.initial)
+
+    useEffect(() => {
+        if(status === 'pending') {
+            const timer = setTimeout(() => {
+                dispatch({type: 'SUCCESS'})
+            }, 1500);
+            return () => {
+                clearTimeout(timer);
+            };
+        }
+    }, [status]);
 
   return (
     <div className="scratch">
@@ -14,7 +50,7 @@ export const ScratchApp = () => {
             minute: '2-digit',
           })}
         </div>
-        <div className="alarmToggle" data-active={isActive || undefined} onClick={() => setIsActive(!isActive)}></div>
+        <div className="alarmToggle" style={{opacity: status === "pending" ? .5 : 1}} data-active={status === 'active' ? 'true' : undefined} onClick={() => dispatch({type: 'TOGGLE'})}></div>
       </div>
     </div>
   );
